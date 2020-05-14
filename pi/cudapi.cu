c4b81074b1c57df1d639856a30797327b810b959
Sarah Joyce <sjoyce@ubuntu-1804.udel.edu>
2020-05-14 15:53:14 -0400

    progress

--- pi/cudapi.cu
+++ pi/cudapi.cu
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 #include <assert.h>
 #include <cuda_runtime.h>
-#define INTERVALS 5000000000L
+#define INTERVALS 5000000000
+#define nblocks 10
+#define nthreads 10
 
-int nthreads;
-int nblocks;
 double num_pi = 0.0;
 
 __global__ void pi(double *area, int threads, int blocks){
@@ -20,45 +20,34 @@ __global__ void pi(double *area, int threads, int blocks){
 }
 
 int main(int argc, char **argv) {
-  assert(argc==3);
-
-  int arg1 = (int)atoi(argv[1]);
-  int arg2 = (int)atoi(argv[2]);
-  //int arg3 = (int)atoi(argv[3]);
-
-  printf("Arg1: %d", arg1);
-  printf("Arg2: %d", arg2);
-  //printf("Arg3: %d", arg3);
-
   clock_t start_time = clock();
 
-  nblocks = (int)atoi(argv[1]);
-  nthreads = (int)atoi(argv[2]);
+  //nblocks = (int)atoi(argv[1]);
+  //nthreads = (int)atoi(argv[2]);
 
-  dim3 numBlocks(nblocks, 1, 1);
-  dim3 threadsPerBlock(nthreads, 1, 1);
+  dim3 numBlocks(nblocks);
+  dim3 threadsPerBlock(nthreads);
 
   double *area;
   double *d_area;
-  area = (double *)malloc(sizeof(double));
+  area = (double *)malloc(nblocks*nthreads*sizeof(double));
   for(int i=0; i<nblocks*nthreads; i++){
-    area[i] = 0;
+    area[i]=0;
   }
 
   cudaMalloc((void **) &d_area, nblocks*nthreads*sizeof(double));
 
-  cudaMemcpy(d_area, &area, nblocks*nthreads*sizeof(double), cudaMemcpyHostToDevice);
+  cudaMemcpy(d_area, area, nblocks*nthreads*sizeof(double), cudaMemcpyHostToDevice);
 
   pi<<<numBlocks, threadsPerBlock>>>(d_area, nthreads, nblocks);
-  
-  cudaDeviceSynchronize();
 
   cudaMemcpy(&area, d_area, nblocks*nthreads*sizeof(double), cudaMemcpyDeviceToHost);
 
   //add everything together
   for(int i=0; i<nblocks*nthreads; i++){
-    num_pi = (num_pi + area[i])*(1.0/INTERVALS);
+    num_pi += area[i];
   }
+  num_pi = num_pi * (1.0/INTERVALS);
 
   clock_t finish_time = clock();
   double time = (double)(finish_time-start_time)/CLOCKS_PER_SEC;
